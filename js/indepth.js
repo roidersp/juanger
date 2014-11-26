ade1feb261b3bf200756e740e4555938f088ddc5
Rodrigo Luis Santander <roidersp@gmail.com>
2014-11-26 09:28:02 -0600

    dsdfsdfsd

--- js/indepth.js
+++ js/indepth.js
@@ -141,11 +141,14 @@ $(document).ready(function(){
 	if(!mobile){
 		
 		var dta_ventana=ventana_alto-60-(ventana_alto*.20);
+		$('#indepth_cover').css("height",(ventana_alto-60-(ventana_alto*.20))+"px");
 		$('#indepth_cover').attr("data-0","height:"+(dta_ventana)+"px;margin-top:0px;");
 		$('#indepth_cover').attr("data-50","height:"+(dta_ventana*.75)+"px;margin-top:50px;");
 		$('#indepth_cover').attr("data-100","height:"+(dta_ventana*.50)+"px;margin-top:100px;");
 		$('#indepth_cover').attr("data-1500","height:"+(dta_ventana*.60)+"px;margin-top:150px;");
 		indepth_skrllr();
+	}else{
+		$('#indepth_cover').css("height",(ventana_alto-60)+"px");
 	}
 	indepth_preloadImgs();
 	
@@ -154,7 +157,7 @@ $(document).ready(function(){
     
 			 $('#indepth_cover_view').css("position","absolute");
     }else{
-    	$('#indepth_cover').css("height",(ventana_alto-60)+"px");
+    	
     	 if(ventana_alto>600){
 	 	$('.indepth_break').css("height",(ventana_alto*.80)+"px");
 
@@ -172,9 +175,9 @@ $(document).ready(function(){
     }
 		loadDisqus($("#indepth_coments"),disqus_url, "http://juanfutbol.com/indepth/"+disqus_url);
 		var ventana_alto = $(window).height();
-    	$('#indepth_cover').css("height",(ventana_alto-60-(ventana_alto*.20))+"px");
+    	
     	 if(ventana_alto>600){
-	 	$('.indepth_break').css("height",(ventana_alto*1.80)+"px");
+	 	
 	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
  	}
 		
@@ -196,10 +199,16 @@ $(window).on("resize", function(){
     }
     
     var ventana_alto = $(window).height();
-    	$('#indepth_cover').css("height",(ventana_alto-60-(ventana_alto*.20))+"px");
-    	console.log((ventana_alto-60)+"px")
-    	 if(ventana_alto>600){
-	 	$('.indepth_break').css("height",(ventana_alto*1.80)+"px");
-	 	$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
-	 	}
+	var mobile=detect_mobile();
+	if(!mobile){
+		var dta_ventana=ventana_alto-60-(ventana_alto*.20);
+		$('#indepth_cover').css("height",(ventana_alto-60-(ventana_alto*.20))+"px");
+		$('#indepth_cover').attr("data-0","height:"+(dta_ventana)+"px;margin-top:0px;");
+		$('#indepth_cover').attr("data-50","height:"+(dta_ventana*.75)+"px;margin-top:50px;");
+		$('#indepth_cover').attr("data-100","height:"+(dta_ventana*.50)+"px;margin-top:100px;");
+		$('#indepth_cover').attr("data-1500","height:"+(dta_ventana*.60)+"px;margin-top:150px;");
+		indepth_skrllr();
+	}else{
+		$('#indepth_cover').css("height",(ventana_alto-60)+"px");
+	}
 })
